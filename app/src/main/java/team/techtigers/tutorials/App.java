aeb793fe07a875f464d1bfcc7b2c1358e47859b2
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2024-08-20 16:45:30 -0400

    Remove all code - empty project

--- app/src/main/java/team/techtigers/tutorials/App.java
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -4,15 +4,7 @@
 package team.techtigers.tutorials;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
-    public boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello, world!");
     }
 }
