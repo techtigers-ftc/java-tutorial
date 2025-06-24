1829cd038b86c8c84e7f5bb2d456caefcb86da63
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-06-24 21:59:36 +0200

    Demonstrate basic type casting

--- app/src/main/java/team/techtigers/tutorials/App.java
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -24,6 +24,48 @@ public class App {
      * @param args Arguments passed to the entry point from the command line.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        short shortVal = Short.MAX_VALUE;
+        int intVal = Integer.MAX_VALUE;
+        long longVal = Long.MAX_VALUE;
+        double doubleVal = 22.0 / 7.0;
+        float floatVal = (float) 22 / 7;
+        boolean boolVal = false;
+        char charVal = 'A';
+        byte byteVal = 65;
+        String strVal = "Hello World!";
+
+        System.out.println("---\n");
+
+        System.out.println("long (original)   : " + longVal);
+        System.out.println("long --> short    : " + (short) intVal);
+        System.out.println("long --> int      : " + (int) longVal);
+        System.out.println("---\n");
+
+        System.out.println("short (original)  : " + shortVal);
+        System.out.println("short --> long    : " + (long) shortVal);
+        System.out.println("short --> int     : " + (int) shortVal);
+        System.out.println("---\n");
+
+        System.out.println("int (original)    : " + intVal);
+        System.out.println("int --> long      : " + (long) intVal);
+        System.out.println("---\n");
+
+        System.out.println("float (original)  : " + floatVal);
+        System.out.println("float --> double  : " + (double) floatVal);
+        System.out.println("---\n");
+
+        System.out.println("double (original) : " + doubleVal);
+        System.out.println("double --> float  : " + (float) doubleVal);
+        System.out.println("---\n");
+
+        System.out.println("char (original)  : " + charVal);
+        System.out.println("char --> short   : " + (short) charVal);
+        System.out.println("char --> byte    : " + (byte) charVal);
+        System.out.println("---\n");
+
+        System.out.println("byte (original)  : " + byteVal);
+        System.out.println("byte --> short   : " + (short) byteVal);
+        System.out.println("byte --> char    : " + (char) byteVal);
+        System.out.println("---\n");
     }
 }
