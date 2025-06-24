f158cf37edca2fdae7d783b4f54305de83c8a6c1
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-06-24 21:40:48 +0200

    Demonstrate primitive data types

--- app/src/main/java/team/techtigers/tutorials/App.java
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -24,6 +24,35 @@ public class App {
      * @param args Arguments passed to the entry point from the command line.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        short shortVal = Short.MAX_VALUE;
+        int intVal = Integer.MAX_VALUE;
+        long longVal = Long.MAX_VALUE;
+        double doubleVal = 22.0 / 7.0;
+        float floatVal = (float) 22 / 7;
+        boolean boolVal = false;
+        char charVal = 'A';
+        byte byteVal = 65;
+        String strVal = "Hello World!";
+
+        System.out.println("---\n");
+
+        System.out.println("Short value   : " + shortVal);
+        System.out.println("Int value     : " + intVal);
+        System.out.println("Long value    : " + longVal);
+        System.out.println("---\n");
+
+        System.out.println("Float value   : " + floatVal);
+        System.out.println("Double value  : " + doubleVal);
+        System.out.println("---\n");
+
+        System.out.println("Boolean value : " + boolVal);
+        System.out.println("---\n");
+
+        System.out.println("Char value    : " + charVal);
+        System.out.println("Byte value    : " + byteVal);
+        System.out.println("---\n");
+
+        System.out.println("String value  : " + strVal);
+        System.out.println("---\n");
     }
 }
