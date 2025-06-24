e7b2e61b40fc98f2a0ea54c5f1dd8c65225d1194
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-06-24 21:13:31 +0200

    Add "Hello World!" implementation

---
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -0,0 +1,29 @@
+// Packages are units of organization of Java programs.
+// Note: Convention dictates that the package structure typically follows the
+// reverse order of the author's domain name. For example, if the author's
+// domain is "techtigers.team", the package name would be "team.techtigers".
+package team.techtigers.tutorials;
+
+/**
+ * Class for the main application code.
+ * <p>
+ * Note: All code in Java must be defined in classes. This is not defined as a
+ * traditional object oriented class, but is only intended as an entry point
+ * into the program.
+ * </p>
+ */
+public class App {
+
+    /**
+     * Program entrypoint.
+     * <p>
+     * This is a special method that is invoked when the java program is run
+     * from the command line.
+     * </p>
+     *
+     * @param args Arguments passed to the entry point from the command line.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
