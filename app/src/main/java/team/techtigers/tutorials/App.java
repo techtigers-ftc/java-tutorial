cab8ca348a2cd3472bc93d2c0bd4069b4d154606
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-06-25 13:32:49 +0200

    Demonstrate basic operators and assignments

--- app/src/main/java/team/techtigers/tutorials/App.java
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -24,6 +24,92 @@ public class App {
      * @param args Arguments passed to the entry point from the command line.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int intA = 10;
+        int intB = 20;
+        double doubleA = 10.5;
+        double doubleB = 20.5;
+        boolean boolA = true;
+        boolean boolB = false;
+
+        System.out.println("---\n");
+
+        System.out.println("int operations (a, b): (" + intA + ",  " + intB + ")");
+        System.out.println("addition                 a + b  = " + (intA + intB));
+        System.out.println("subtraction              a - b  = " + (intA - intB));
+        System.out.println("multiplication           a * b  = " + (intA * intB));
+        System.out.println("division                 a / b  = " + (intA / intB));
+        System.out.println("modulus                  a % b  = " + (intA % intB));
+        System.out.println("compare equal            a == b = " + (intA == intB));
+        System.out.println("compare not equal        a != b = " + (intA != intB));
+        System.out.println("compare greater          a > b  = " + (intA > intB));
+        System.out.println("compare lesser           a < b  = " + (intA < intB));
+        System.out.println("compare greater or equal a >= b = " + (intA >= intB));
+        System.out.println("compare lesser or equal  a <= b = " + (intA <= intB));
+
+        System.out.println("---\n");
+
+        System.out.println("double operations (a, b): (" + doubleA + ",  " + doubleB + ")");
+        System.out.println("modulus                  a % b  = " + (intA % intB));
+        System.out.println("addition                 a + b  = " + (doubleA + doubleB));
+        System.out.println("subtraction              a - b  = " + (doubleA - doubleB));
+        System.out.println("multiplication           a * b  = " + (doubleA * doubleB));
+        System.out.println("division                 a / b  = " + (doubleA / doubleB));
+        System.out.println("modulus                  a % b  = " + (doubleA % doubleB));
+        System.out.println("compare equal            a == b = " + (doubleA == doubleB));
+        System.out.println("compare not equal        a != b = " + (doubleA != doubleB));
+        System.out.println("compare greater          a > b  = " + (doubleA > doubleB));
+        System.out.println("compare lesser           a < b  = " + (doubleA < doubleB));
+        System.out.println("compare greater or equal a >= b = " + (doubleA >= doubleB));
+        System.out.println("compare lesser or equal  a <= b = " + (doubleA <= doubleB));
+        System.out.println("---\n");
+
+        System.out.println("mixed operations (a, b): (" + doubleA + ",  " + intB + ")");
+        System.out.println("addition                 a + b  = " + (doubleA + intB));
+        System.out.println("subtraction              a - b  = " + (doubleA - intB));
+        System.out.println("multiplication           a * b  = " + (doubleA * intB));
+        System.out.println("division                 a / b  = " + (doubleA / intB));
+        System.out.println("modulus                  a % b  = " + (doubleA % intB));
+        System.out.println("compare equal            a == b = " + (doubleA == intB));
+        System.out.println("compare not equal        a != b = " + (doubleA != intB));
+        System.out.println("compare greater          a > b  = " + (doubleA > intB));
+        System.out.println("compare lesser           a < b  = " + (doubleA < intB));
+        System.out.println("compare greater or equal a >= b = " + (doubleA >= doubleB));
+        System.out.println("compare lesser or equal  a <= b = " + (doubleA <= doubleB));
+        System.out.println("---\n");
+
+        System.out.println("boolean operations (a, b): (" + boolA + ",  " + boolB + ")");
+        System.out.println("AND                    a AND b  = " + (boolA && boolB));
+        System.out.println("OR                     a OR b   = " + (boolA || boolB));
+        System.out.println("XOR                    a XOR b  = " + (boolA ^ boolB));
+        System.out.println("NOT                    NOT a    = " + (!boolA));
+        System.out.println("---\n");
+
+        System.out.println("Assignment statements (a, b): (" + intA + ",  " + intB + ")");
+        int c = intA + intB;
+        System.out.println("c = a + b;                   c  = " + c);
+        c = c + 1;
+        System.out.println("c = c + 1;                   c  = " + c);
+        c += 1;
+        System.out.println("c += 1;                      c  = " + c);
+        c++;
+        System.out.println("c++;                         c  = " + c);
+
+        c = c - 1;
+        System.out.println("c = c - 1;                   c  = " + c);
+        c -= 1;
+        System.out.println("c -= 1;                      c  = " + c);
+        c--;
+        System.out.println("c--;                         c  = " + c);
+
+        c = c * 2;
+        System.out.println("c = c * 2;                   c  = " + c);
+        c *= 2;
+        System.out.println("c *= 2;                      c  = " + c);
+
+        c = c / 2;
+        System.out.println("c = c / 2;                   c  = " + c);
+        c /= 2;
+        System.out.println("c /= 2;                      c  = " + c);
+        System.out.println("---\n");
     }
 }
