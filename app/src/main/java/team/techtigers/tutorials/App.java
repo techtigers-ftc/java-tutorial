d85a9600fe7fc5a78d725163bc97082d64307f66
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-06-27 15:40:45 +0200

    Add exercises to the module

--- app/src/main/java/team/techtigers/tutorials/App.java
+++ app/src/main/java/team/techtigers/tutorials/App.java
@@ -54,5 +54,30 @@ public class App {
 
         System.out.println("String value  : " + strVal);
         System.out.println("---\n");
+
+        /* ---
+         * Exercises:
+         * ----------
+         * 1. What is the smallest value for a short/int/long?
+         *
+         * 2. The integer value is currently set to its maximum value.
+         *    What do you get when you try to increment it (increase its value
+         *    by 1)?
+         *    Note: You can increment a value by using the `++` operator.
+         *    For example, intVal++; will increment the value of intVal by 1.
+         *
+         * 3. What do you get if you now try to decrement it (decrease its value
+         *    by 1)?
+         *    Note: You can decrement a value by using the `--` operator.
+         *
+         * 4. Based on the results of the previous exercises, how many different
+         *    values can you store in a short?
+         *
+         * 5. Explore the the limits and ranges of the other data types.
+         *
+         * 6. You can declare a variable without assigning it a value like this:
+         *      int uninitializedInt;
+         *    What happens if you try to print the value of this variable?
+         * --- */
     }
 }
